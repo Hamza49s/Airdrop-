3669612c661f26fcf3bbe2ca7909e807e4c70fb4
Hamza Aslam <157880646+Hamza49s@users.noreply.github.com>
2024-04-28 16:30:12 +0500

    Update mainfile

--- deployTestingContract.js
+++ deployTestingContract.js
@@ -2,11 +2,11 @@ const { ethers } = require("ethers");
 
 async function deployAndVerify() {
     // Connect to Ethereum network
-    const providerUrl = "https://eth-sepolia.g.alchemy.com/v2/Jj_luMlU_qU-AE5UHvF9pngQFbw6s8wm";
+    const providerUrl = "";
     const provider = new ethers.providers.JsonRpcProvider(providerUrl);
 
     // Set up the signer (wallet) for deploying the contract
-    const privateKey = "c359eec1563db748a9681cfbd2ba2408b28188f52d2cab9f23b688ebf688e47a";
+    const privateKey = "";
     const wallet = new ethers.Wallet(privateKey, provider);
 
     // Load the compiled contract ABI and bytecode
@@ -17,7 +17,7 @@ async function deployAndVerify() {
     // Deploy the contract
     console.log("Deploying Contract...");
     const factory = new ethers.ContractFactory(abi, bytecode, wallet);
-    const contract = await factory.deploy("0x56eC1BDD1DCF9E5cfA8D8A37C21a4CD806c133D4"); // Pass the address of the ERC20 token contract
+    const contract = await factory.deploy(""); // Pass the address of the ERC20 token contract
     await contract.deployed();
     console.log("Contract deployed to:", contract.address);
 
